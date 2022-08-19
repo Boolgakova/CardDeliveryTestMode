fbfc65b7e7cbcc3eedbc82bf04ce8e0747fa8b6a
Boolgakova <boolgakova@gmail.com>
2022-08-19 20:31:48 +0300

    Headless added

--- src/test/java/ru/netology/ibank/test/AuthTest.java
+++ src/test/java/ru/netology/ibank/test/AuthTest.java
@@ -1,6 +1,7 @@
 package ru.netology.ibank.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,7 @@ class AuthTest {
 
     @BeforeEach
     void setup() {
+        Configuration.headless = true;
         open("http://localhost:9999");
     }
 
